69360dbd9a8abd5e87a38980216c7e0c1997dd2e
ghiles1995 <2417133@grasset.qc.ca>
2025-02-21 13:44:33 -0500

    first commit

---
+++ src/main/java/com/mycompany/examenfinalpoo/ExamenFinalpoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenfinalpoo;
+
+/**
+ *
+ * @author 2417133
+ */
+public class ExamenFinalpoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
